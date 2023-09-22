cc8b8ec205a572b1abcc54bada080d90bf9e02f4
AmparoFernández <amparip27@gmail.com>
2023-09-22 13:53:06 +0200

    feat(chapter 1-block 1): changes on exercise 1

--- src/capitulo01/bloque01/Ejercicio01_PideTresNumerosYMuestraEnConsola.java
+++ src/capitulo01/bloque01/Ejercicio01_PideTresNumerosYMuestraEnConsola.java
@@ -3,8 +3,7 @@ package capitulo01.bloque01;
 public class Ejercicio01_PideTresNumerosYMuestraEnConsola {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("Hola Mundo");
 	}
 
 }
