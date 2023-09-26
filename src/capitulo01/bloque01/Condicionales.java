d2b81128e5503a7308bf5cfe92e6382ba068b6e9
AmparoFernández <amparip27@gmail.com>
2023-09-26 12:47:27 +0200

    feat (ch 2- Condicionales ex): added

--- src/capitulo01/bloque01/Condicionales.java
+++ src/capitulo01/bloque01/Condicionales.java
@@ -1,9 +1,65 @@
 package capitulo01.bloque01;
 
-public class Ejercicio01_PideTresNumerosYMuestraEnConsola {
+import java.util.Scanner;
+
+public class Condicionales {
+
+
+
+//	public static void main(String[] args) {
+//		Scanner sc = new Scanner(System.in);
+		
+//		System.out.println("Dame un número: ");
+//		int num1 = sc.nextInt();
+		
+//		System.out.println("Dame otro número: ");
+//		int num2 = sc.nextInt();
+		
+//	if (num1 > num2) {
+//		System.out.println("El número mayor es: " +num1);
+//		System.out.println("El número menor es: " +num2);
+//		}
+//	else {
+//		System.out.println("El número mayor es: " +num2);
+//		System.out.println("El número menor es: " +num1);
+//	}
+//	}}
+
+//public static void main(String[] args) {
+//	Scanner sc = new Scanner(System.in);
+	
+//	System.out.println("Dame un número: ");
+//	int num3 = sc.nextInt();
+	
+//	System.out.println("Dame otro número(2): ");
+//	int num4 = sc.nextInt();
+	
+//	System.out.println("Dame otro número(3): ");
+//	int num5 = sc.nextInt();
+	
+//	System.out.println("Dame otro número(4): ");
+//	int num6 = sc.nextInt();
+	
+//	System.out.println("Dame otro número(5): ");
+//	int num7 = sc.nextInt();
+	
+//	System.out.println("el numero mayor es: " + Math.max( (Math.max(num3, num4) ),(Math.max( (Math.max(num5, num6) ),num7))));
+//	System.out.println("el numero menor es: " + Math.min( (Math.min(num3, num4) ),(Math.min( (Math.min(num5, num6) ),num7))));
+//}
+//}
 
 	public static void main(String[] args) {
-		System.out.println("Hola Mundo");
+		Scanner sc = new Scanner(System.in);
+	
+		System.out.println("Dame un número: ");
+		int num8 = sc.nextInt();
+	
+		if (num8 % 2 == 0) {
+		System.out.println("el número es par");
+		}
+		else {
+		System.out.println("el número es impar");
+		}
+	
 	}
-
 }
