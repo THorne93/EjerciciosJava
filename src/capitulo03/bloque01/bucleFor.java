c070159edc21677318ad78a454b51db54f0fbda6
THorne93 <taylorhorne1993@gmail.com>
2023-10-02 12:48:20 +0200

    feat (ex hecho)

---
+++ src/capitulo03/bloque01/bucleFor.java
@@ -0,0 +1,42 @@
+package capitulo03.bloque01;
+
+import javax.swing.JOptionPane;
+
+public class bucleFor{
+
+
+		public static void main(String[] args) {
+			String str;
+			int sum = 0;
+			int pos = 0;
+			int neg = 0;
+			str = JOptionPane.showInputDialog("Cuántos números vas a pedir?");
+			int tot  = Integer.parseInt(str);
+			
+				for (int i = 0; i < tot; i++) {
+					if (i == 0) {str = JOptionPane.showInputDialog("Introduzca un número");
+					int num = Integer.parseInt(str);
+						if (num >= 0) {
+							if (num >= 10) {
+								sum = sum + num;
+								pos++;
+							}
+							else pos++;
+						}
+						else neg++;	
+						}
+					else {str = JOptionPane.showInputDialog("Introduzca un número");
+					int num = Integer.parseInt(str);
+						if (num >= 0) {
+							if (num >= 10) {
+								sum = sum + num;
+								pos++;
+							}
+							else pos++;
+						}
+						else neg++;	
+						}
+					}
+				JOptionPane.showInternalMessageDialog(null, "Hay " + pos + " positivos y " + neg +" negativos. El total es: " +sum);
+				}
+		}
