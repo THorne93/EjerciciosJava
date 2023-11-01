705b253dee3bfc03b787ab88253829c7f29b4a28
THorne93 <taylorhorne1993@gmail.com>
2023-11-01 09:54:43 +0100

    feat(commit): formatted java doc

--- src/capitulo04/bloque01/StringParaExpertos.java
+++ src/capitulo04/bloque01/StringParaExpertos.java
@@ -4,89 +4,89 @@ public class StringParaExpertos {
 
 	public static void main(String[] args) {
 		String string1 = Utils.Arrays.requestString("Dime algo");
-//		String string1 = "Hola que tal vosotros";
 
 		// ex01
 //		System.out.print(eliminarEspaciosEnBlanco(string1));
 
-		//ex02
+		// ex02
 //		cifradoCesar(string1);
 
-		//ex03
+		// ex03
 //		contarOcurrencias(string1);
 
-		//ex04
+		// ex04
 //		encontrarPalabraMasLarga(string1);
-		
-		//ex05
+
+		// ex05
 //		System.out.print(intertirContenidoPalabras(string1));
-		
-		//ex06
+
+		// ex06
 		buscarPangrama(string1);
-		}
-
-
-
+	}
 
 	private static void buscarPangrama(String string1) {
 		String alfabeto = "abcdefghijklmnopqrstuvwxyz";
 		char[] stringAlfabeto = alfabeto.toCharArray();
-		
+
 		String stringx = capitulo04.bloque01.ArraysDeCaracteresYString.textoMeniscula(string1);
 
 		stringx = eliminarEspaciosEnBlanco(stringx);
-		
+
 		char[] stringPrueba = stringx.toCharArray();
 		int j;
 		boolean esPangrama = true;
-		
+
 		for (int i = 0; i < alfabeto.length(); i++) {
-			
-			 boolean found = false;
-			    for (j = 0; j < stringx.length(); j++) {
-			        if (stringPrueba[j] == stringAlfabeto[i]) {
-			            found = true;
-			            break;
-			        }
-			    }
-			    if (!found) {
-			        esPangrama = false;
-			    }
+
+			boolean found = false;
+			for (j = 0; j < stringx.length(); j++) {
+				if (stringPrueba[j] == stringAlfabeto[i]) {
+					found = true;
+					break;
+				}
 			}
-			
-		if (esPangrama == true) System.out.println(string1 +" Es una pangrama");
-		else System.out.println(string1 +" No es una pangrama");
+			if (!found) {
+				esPangrama = false;
+			}
+		}
+
+		if (esPangrama == true)
+			System.out.println(string1 + " Es una pangrama");
+		else
+			System.out.println(string1 + " No es una pangrama");
 	}
 	/*
 	 * 
-	 */	
-	
+	 */
+
 	private static String intertirContenidoPalabras(String string1) {
 		int posicionPalabra = 0;
 		int posicionPalabraFinal = 0;
 		int j = 0;
 		int limit = string1.length();
 		String stringx = ("");
-		
+
 		for (int i = 0; i < string1.length(); i++) {
-			
-			if ((string1.charAt(i) >= 65 && string1.charAt(i) <= 90) || (string1.charAt(i) >= 97 && string1.charAt(i) <= 122)) {
+
+			if ((string1.charAt(i) >= 65 && string1.charAt(i) <= 90)
+					|| (string1.charAt(i) >= 97 && string1.charAt(i) <= 122)) {
 				posicionPalabra = i;
 				j = i;
-				
-				//  (string1.charAt(j) !=32 && j < limit) no funciona, pero al reves si
+
+				// (string1.charAt(j) !=32 && j < limit) no funciona, pero al reves si
 				while (j < limit && string1.charAt(j) != 32) {
 					j++;
 				}
 
-				posicionPalabraFinal = j-1;
+				posicionPalabraFinal = j - 1;
 				for (int k = posicionPalabraFinal; k >= posicionPalabra; k--) {
 					stringx += string1.charAt(k);
 				}
 				i = posicionPalabraFinal;
 			}
-			
-			else stringx += " ";
+
+			else
+				stringx += " ";
 		}
 		return stringx;
 	}
